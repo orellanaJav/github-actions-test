404ffdcf613a2f732fd9a3458976812d34c17ab4
Javier Orellana <javier.orellana@destacame.cl>
2022-06-01 11:08:54 -0400

    Added new step

--- stack_project_1/services/project_1/lambda_1/lambda_1.py
+++ stack_project_1/services/project_1/lambda_1/lambda_1.py
@@ -1,24 +1,6 @@
-import json
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def get_formatted_name(first, middle, last):
+    formatted_name = f'{first} {middle} {last}'
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    return formatted_name
---
+++ stack_project_1/services/project_1/lambda_1/test_name_function.py
@@ -0,0 +1,17 @@
+import unittest
+from lambda_1 import get_formatted_name
+
+
+class NamesTestCase(unittest.TestCase):
+
+    # def test_first_last_name(self):
+    #     formatted_name = get_formatted_name('Janis', 'Joplin')
+    #     self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_middle_last_name(self):
+        formatted_name = get_formatted_name('Michael', 'J', 'Fox')
+        self.assertEqual(formatted_name, 'Michael J Fox')
+
+
+if __name__ == '__main__':
+    unittest.main()
