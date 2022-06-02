1de1007972e7c750eb7332fa3aa5590dc1b39b92
Javier Orellana <javier.orellana@destacame.cl>
2022-06-02 11:04:02 -0400

    Test function deploy 1

--- stack_project_1/services/project_1/lambda_1/lambda_1.py
+++ stack_project_1/services/project_1/lambda_1/lambda_1.py
@@ -2,6 +2,7 @@
 
 def get_formatted_name(first, middle, last):
     print('test function')
+    print('test function deploy')
     formatted_name = f'{first} {middle} {last}'
 
     return formatted_name
