8421b1818aa9a8402c1d2e418d7bc627abe7da21
Javier Orellana <javier.orellana@destacame.cl>
2022-06-02 17:40:00 -0400

    Changed files

--- stack_project_1/services/project_1/lambda_1/lambda_1.py
+++ stack_project_1/services/project_1/lambda_1/lambda_1.py
@@ -1,9 +1,7 @@
 
 
 def get_formatted_name(first, middle, last):
-    print('test function')
-    print('test function')
-    print('test function')
+    print('test function ---> ')
     formatted_name = f'{first} {middle} {last}'
 
     return formatted_name
