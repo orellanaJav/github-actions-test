c51ac48e75685f31748590c82479b02126544d45
Javier Orellana <javier.orellana@destacame.cl>
2022-06-03 15:01:54 -0400

    Added new stack

---
+++ stack_project_2/services/project_2/lambda_1/lambda_1.py
@@ -0,0 +1,9 @@
+def handler():
+    print('cambio para deploy update serverless')
+    print('cambio para deploy update serverless')
+    print('cambio para deploy update serverless')
+    print('cambio para deploy update serverless')
+    print('cambio para deploy update serverless')
+    print('cambio para deploy update serverless')
+    print('cambio para deploy update serverless')
+    print('cambio para deploy update serverless')
